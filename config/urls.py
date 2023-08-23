a133f665c6a8540fda59ed709735389a16636369
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-23 23:09:00 +0300

    add urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('habit_tracker.urls', namespace='habit_tracker')),
-    path('users/', include('users.urls', namespace='users')),
+    path('api/', include('habit_tracker.urls', namespace='habit_tracker')),
+    path('', include('users.urls', namespace='users')),
 ]
