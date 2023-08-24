634fc9b16c868a16262592f9d5a0dbd0e1ca12e2
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-24 10:53:16 +0300

    add admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'phone', 'is_active',)
+    list_filter = ('id',)
