391824e9d4cfeef154e4233a99632e5402b77c4a
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-24 09:25:28 +0300

    add validators

--- habit_tracker/validators.py
+++ habit_tracker/validators.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import time
 
 from rest_framework import serializers
 
@@ -7,10 +7,13 @@ class HabitValidator:
     def __call__(self, value):
         if value.get('related_habit') and value.get('reward'):
             raise serializers.ValidationError('Нельзя одновременно указывать связанную привычку и вознаграждение')
-        if value.get('execution_time') > datetime.time(hour=0, minute=2, second=0):
+        if value.get('execution_time') > time(00, 2):
             raise serializers.ValidationError('Время выполнения должно быть не больше 120 секунд')
         if value.get('related_habit') and not value.get('related_habit').is_pleasant:
             raise serializers.ValidationError(
                 'В связанные привычки могут попадать только привычки с признаком приятной привычки')
+        if value.get('related_habit') and value.get('reward'):
+            raise serializers.ValidationError(
+                'У приятной привычки не может быть вознаграждения или связанной привычки.')
         if value.get('frequency') > 7:
             raise serializers.ValidationError('Нельзя выполнять привычку реже, чем 1 раз в 7 дней')
