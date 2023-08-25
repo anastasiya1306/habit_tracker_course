2651ab95cdb4ef1b619481bbbc91cabe595d5838
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-25 05:40:13 +0300

    add habit_tracker

--- habit_tracker/tasks.py
+++ habit_tracker/tasks.py
@@ -6,12 +6,12 @@ from habit_tracker.models import Habit
 
 
 @shared_task
-def habit_task():
+def habit_task(habit):
     bot = telebot.TeleBot(settings.TELEGRAM_BOT_API)
     time_now = datetime.now()
     time_start_task = time_now - timedelta(minutes=1)
     habit_data = Habit.objects.filter(time__gte=time_start_task)
 
     for item in habit_data.filter(time__lte=time_now):
-        message = f"В {item.place} не забудьте {item.action}"
+        message = f"В {item.place} не забудьте {item.action} в {item.time}"
         bot.send_message(message, 'item.user.chat_id')
